7d7f543a6e67b208da98a0975e724c67b03a0d05
RayhanHafidh <rayhanhafidh@gmail.com>
2022-12-14 23:27:20 +0700

    ngubah task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://main--superlative-meerkat-c9590f.netlify.app/" // TODO: replace this
 const studentName = "Rayhan Hafidh WIarso" // TODO: replace this
 const studentId = "FE2717527" // TODO: replace this
 
